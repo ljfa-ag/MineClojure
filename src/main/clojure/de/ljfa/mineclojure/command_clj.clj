900854b808e64000fb9829032015095c51360ec7
ljfa <ljfa-ag@web.de>
2015-10-09 22:19:02 +0200

    Create refs for *1, *2, *3 and *e

--- src/main/clojure/de/ljfa/mineclojure/command_clj.clj
+++ src/main/clojure/de/ljfa/mineclojure/command_clj.clj
@@ -23,15 +23,27 @@
 (binding [*ns* repl-ns]
   (use 'clojure.core))
 
-(.setDynamic (intern repl-ns 'me)) ;this gets bound to the command sender
+(.setDynamic (intern repl-ns 'me)) ;gets bound to the command sender
+
+;get bound to *1, *2, *3 and *e
+(def r1 (ref nil))
+(def r2 (ref nil))
+(def r3 (ref nil))
+(def re (ref nil))
 
 (defn -processCommand
   [this sender args]
   (try
     (let [in-str (clojure.string/join " " args)
           output (with-open [wr (chat-writer sender)]
-                   (binding [*ns* repl-ns, *out* wr, mineclj/me sender]
+                   (binding [*ns* repl-ns, *out* wr, mineclj/me sender,
+                             *1 @r1, *2 @r2, *3 @r3, *e @re]
                      (eval (read-string in-str))))]
-      (send-chat-lines sender (str-nil output)))
+      (send-chat-lines sender (str-nil output))
+      (dosync
+        (ref-set r3 @r2)
+        (ref-set r2 @r1)
+        (ref-set r1 output)))
     (catch Throwable e
+      (dosync (ref-set re e))
       (throw (CommandException. (str e) (into-array []))))))
